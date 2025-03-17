d74c29f0231fbbc57026c974ac0dcb265fb37959
dangmquann <dangquancmt2003@gmail.com>
2025-03-17 17:34:12 +0700

    script run middle kernel v2

--- experiments/tuningPET.sh
+++ experiments/tuningPET.sh
@@ -4,29 +4,29 @@ export PYTHONPATH=/Users/macbook/Documents/WorkSpace/DeepKernelLearning:$PYTHONP
 
 python tuning.py  --modality PET --version 3 \
             --kernel_type rbf \
-            --epochs 1000 --dropout_rate 0.1 --patience 200 \
+            --epochs 30000 --dropout_rate 0.1 --patience 3000 \
             --batch_size 64 --lr 1e-4 --lambda_ 0.75 \
             --hidden_dims 1000 60 \
             --latent_dim 30 
 
-# python tuning.py  --modality PET --version 4 \
-#             --kernel_type rbf \
-#             --epochs 1000 --dropout_rate 0.1 --patience 200 \
-#             --batch_size 64 --lr 1e-3 --lambda_ 0.7 \
-#             --hidden_dims 1000 60 \
-#             --latent_dim 30 
+python tuning.py  --modality PET --version 4 \
+            --kernel_type rbf \
+            --epochs 30000 --dropout_rate 0.1 --patience 5000 \
+            --batch_size 64 --lr 1e-4 --lambda_ 0.75 \
+            --hidden_dims 1000 60 \
+            --latent_dim 30 
 
 # python tuning.py  --modality PET --version 5 \
 #             --kernel_type rbf \
 #             --epochs 1000 --dropout_rate 0.1 --patience 200 \
-#             --batch_size 64 --lr 1e-5 --lambda_ 0.7 \
+#             --batch_size 64 --lr 1e-4 --lambda_ 0.9 \
 #             --hidden_dims 1000 60 \
 #             --latent_dim 30 
 
 # python tuning.py  --modality PET --version 6 \
 #             --kernel_type rbf \
 #             --epochs 1000 --dropout_rate 0.1 --patience 200 \
-#             --batch_size 32 --lr 1e-4 --lambda_ 0.7 \
+#             --batch_size 32 --lr 1e-4 --lambda_ 0.95 \
 #             --hidden_dims 1000 60\
 #             --latent_dim 30 \
 
